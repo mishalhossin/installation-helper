75dba03e0eefe5f05ad807df36b563cfbeed435c
Mishal <91066601+mishalhossin@users.noreply.github.com>
2023-05-26 17:13:18 +0600

    Create installer_repl.sh

---
+++ installer_repl.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Welcome message with ASCII art
+echo "Welcome to the Discord AI Chatbot installation script!"
+echo ""
+echo "   █ █▄░█ █ ▀█▀ ▄▀█ █░░ █░░ █▀▀ █▀█"
+echo "   █ █░▀█ █ ░█░ █▀█ █▄▄ █▄▄ ██▄ █▀▄"
+echo ""
+echo "This script will guide you through the installation process."
+echo ""
+
+# Check if main.py already exists and delete it if necessary
+if [ -f main.py ]; then
+    echo "Deleting existing main.py..."
+    rm main.py
+    echo "Existing main.py deleted!"
+    echo ""
+fi
+
+# Clone the repository
+echo "Cloning the Discord AI Chatbot repository..."
+git clone https://github.com/mishalhossin/Discord-AI-Chatbot .
+echo "Cloning complete!"
+echo ""
+
+# Prompt user for Discord bot token
+echo "Please provide your Discord bot token."
+read -p "Discord Bot Token: " discord_token
+echo ""
+
+# Prompt user for Hugging Face access token
+echo "Please provide your Hugging Face access token."
+read -p "Hugging Face Access Token: " hf_access_token
+echo ""
+
+# Set environment variables for Discord bot token and Hugging Face access token
+echo "Setting environment variables..."
+export HUGGING_FACE_API=$hf_access_token
+export DISCORD_TOKEN=$discord_token
+echo "Environment variables set!"
+echo ""
+
+# Change directory to the correct location
+cd "path/to/main.py"  # Replace "path/to/main.py" with the actual path to your main.py file
+echo "Changed directory to the correct location!"
+echo ""
+
+# Run the bot
+echo "Running the Discord AI Chatbot..."
+python main.py
